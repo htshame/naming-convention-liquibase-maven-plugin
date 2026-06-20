48c9b647cde35eb275b47ab66d8bb2a670a11c92
htshame <htshame@gmail.com>
2026-06-20 17:56:46 -0400

    Refactor gateway tests

--- ncl-core/src/test/java/io/github/htshame/gateway/ConfigApiGatewayTest.java
+++ ncl-core/src/test/java/io/github/htshame/gateway/ConfigApiGatewayTest.java
@@ -1,9 +1,13 @@
 package io.github.htshame.gateway;
 
+import com.sun.net.httpserver.HttpServer;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -12,39 +16,43 @@ import java.net.URL;
  */
 public class ConfigApiGatewayTest {
 
+    private static final int OK_200 = 200;
+
     /**
      * Test getting file from URL. Http with redirect.
      *
-     * @throws MalformedURLException - malformed URL exception.
      */
     @Test
-    public void testGetFileHttp() throws MalformedURLException {
+    public void testGetFileHttp() {
         // arrange
-        URL url = new URL("http://raw.githubusercontent.com/htshame/naming-convention-liquibase-maven-plugin/"
-                + "refs/heads/main/docs/schema/example/rules_example.xml");
+        HttpServer server;
+        try {
+            server = HttpServer.create(new InetSocketAddress(0), 0);
+        } catch (IOException e) {
+            throw new AssertionError("Failed to start test HTTP server", e);
+        }
+        server.createContext("/rules.xml", exchange -> {
+            byte[] body = "<rules/>".getBytes(java.nio.charset.StandardCharsets.UTF_8);
+            exchange.getResponseHeaders().add("Content-Type", "application/xml");
+            exchange.sendResponseHeaders(OK_200, body.length);
+            try (OutputStream os = exchange.getResponseBody()) {
+                os.write(body);
+            }
+        });
+        server.start();
+        try {
+            URL url = new URL("http://localhost:" + server.getAddress().getPort() + "/rules.xml");
 
-        // act
-        File actual = new ConfigApiGateway().getFile(url);
+            // act
+            File actual = new ConfigApiGateway().getFile(url);
 
-        // assert
-        Assert.assertNotNull(actual);
-    }
-
-    /**
-     * Test getting file from URL. Https.
-     *
-     * @throws MalformedURLException - malformed URL exception.
-     */
-    @Test
-    public void testGetFileHttps() throws MalformedURLException {
-        // arrange
-        URL url = new URL("https://raw.githubusercontent.com/htshame/naming-convention-liquibase-maven-plugin/"
-                + "refs/heads/main/docs/schema/example/rules_example.xml");
-
-        // act
-        File actual = new ConfigApiGateway().getFile(url);
-
-        // assert
-        Assert.assertNotNull(actual);
+            // assert
+            Assert.assertTrue(actual.exists());
+            Assert.assertTrue(actual.length() > 0);
+        } catch (MalformedURLException e) {
+            throw new AssertionError(e);
+        } finally {
+            server.stop(0);
+        }
     }
 }
